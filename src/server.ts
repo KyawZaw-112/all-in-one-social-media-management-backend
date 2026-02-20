c5cce5c5f2d6f142344197fbea1b7429603fe720
KyawZaw-112 <kyawzawwin.2001.cs@gmail.com>
2026-02-20 20:21:29 +0700

    fix:cors vercel

--- src/server.ts
+++ src/server.ts
@@ -24,6 +24,7 @@ app.use(cors({
         "http://localhost:3000",
         "https://ashy.vercel.app",
         "https://all-in-one-social-media-management-ashy.vercel.app",
+        "https://all-in-one-social-media-management-frontend-eta.vercel.app",
         process.env.FRONTEND_URL || ""
     ].filter(Boolean),
     credentials: true,
